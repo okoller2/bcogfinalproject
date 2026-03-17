10430d5c878ef026b9dc8dd7c3c70ab418b7e34d
okoller2 <okoller2@illinois.edu>
2026-03-17 16:36:29 -0500

    Add files via upload

---
+++ last_project_code.py
@@ -0,0 +1,33 @@
+
+import random
+
+class get_word:
+    def __init__(self):
+        self.words = ["python", "programming", "lab", "cognitive", "science"]
+
+    def random_word(self):
+        return random.choice(self.words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ = = "__main__":
+    
